bc77aedb3dc48321b9e9c87a80f65574a4ae8d7c
johan <johan@LAPTOP-MO435CIQ.Home>
2017-09-20 16:37:38 +0200

    Added gradle

--- template_project_gradle-master/gradlew.bat
+++ template_project_gradle-master/gradlew.bat
@@ -19,7 +19,7 @@ set APP_HOME=%DIRNAME%
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-set JAVA_EXE=java.exe
+set JAVA_EXE=C:\Program Files\Java\jdk1.8.0_131\bin\java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
 
@@ -32,7 +32,7 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131\bin
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
